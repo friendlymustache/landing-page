4c3bcea2157ed16e8fd8d89ab0f6c342d3580978
Siddharth Murching <smurching@caltech.edu>
2015-07-13 17:20:17 -0700

    Add high schooler and college student option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 	skip_before_action :verify_authenticity_token
 	def create
 		@user = User.create(user_params)
-		puts "Created user!"
 	    # SendEmailJob.set(wait: 3.seconds).perform_later(@user)
 		UserMailer.welcome_email(@user).deliver_now
 		render :nothing => true
